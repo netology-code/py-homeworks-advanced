84e454569a144bcc438ec7e4e2d8988eceb2bce0
eshmargunov <eshmargunov@gmail.com>
2020-10-14 23:00:43 +0300

    update hw for tests

--- 4.Tests/src/app.py
+++ 4.Tests/src/app.py
@@ -1,17 +1,14 @@
-# Домашнее задание к лекции 2.1 «Функции — использование встроенных и создание собственных»
-import json
-import os
+documents = [
+    {"type": "passport", "number": "2207 876234", "name": "Василий Гупкин"},
+    {"type": "invoice", "number": "11-2", "name": "Геннадий Покемонов"},
+    {"type": "insurance", "number": "10006", "name": "Аристарх Павлов"}
+]
 
-
-def update_date():
-    current_path = str(os.path.dirname(os.path.abspath(__file__)))
-    f_directories = os.path.join(current_path, 'fixtures/directories.json')
-    f_documents = os.path.join(current_path, 'fixtures/documents.json')
-    with open(f_documents, 'r') as out_docs:
-        documents = json.load(out_docs)
-    with open(f_directories, 'r') as out_dirs:
-        directories = json.load(out_dirs)
-    return directories, documents
+directories = {
+    '1': ['2207 876234', '11-2', '5455 028765'],
+    '2': ['10006'],
+    '3': []
+}
 
 
 def check_document_existance(user_doc_number):
@@ -174,6 +171,5 @@ def secretary_program_start():
             break
 
 
-directories, documents = update_date()
 if __name__ == '__main__':
     secretary_program_start()
